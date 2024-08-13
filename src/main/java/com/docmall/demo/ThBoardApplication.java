d04a00bccc4b8a63352c6e39fb4f19faad850da8
doccomsa <newcomsa@nate.com>
2024-08-13 22:50:10 +0900

    deploy 1

--- src/main/java/com/docmall/demo/ThBoardApplication.java
+++ src/main/java/com/docmall/demo/ThBoardApplication.java
@@ -3,8 +3,6 @@ package com.docmall.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,11 +15,6 @@ public class ThBoardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ThBoardApplication.class, args);
 	}
-	
-	@GetMapping("/")
-	public String root() {
-		log.info("root");
-		return "redirect:/board/list";
-	}
+
 
 }
