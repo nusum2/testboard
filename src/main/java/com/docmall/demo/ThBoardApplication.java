beee0e5f23e7358ac5d36e783edc5aef156ca70f
nusum2 <warfare804@gmail.com>
2024-08-16 14:43:51 +0900

    deploy ok

--- src/main/java/com/docmall/demo/ThBoardApplication.java
+++ src/main/java/com/docmall/demo/ThBoardApplication.java
@@ -4,10 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
 
-
-@Slf4j
 @MapperScan(basePackages = {"com.docmall.demo.mapper"})
 @SpringBootApplication
 public class ThBoardApplication {
